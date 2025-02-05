c52f4b60110c5d4bef7572348add86e50762cc52
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-05 15:39:46 +0700

    Update 7_test.py

--- pages/7_test.py
+++ pages/7_test.py
@@ -3,5 +3,5 @@ import streamlit as st
 file_pdf = st.file_uploader("Upload a PDF", type="pdf")
 
 if file_pdf is not None:
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="400" type="application/pdf">'
+    pdf_display = f'<embed src="data:application/pdf;base64,{file_pdf}" width="700" height="400" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
