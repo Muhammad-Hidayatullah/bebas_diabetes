9c7c066a97d29216f06d03cfc0dae6b95e78ccd5
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-05 15:20:24 +0700

    Update 3_diagnosis_diabetes_tipe_2.py

--- pages/3_diagnosis_diabetes_tipe_2.py
+++ pages/3_diagnosis_diabetes_tipe_2.py
@@ -666,7 +666,7 @@ if st.session_state.next == 100:
                         total_kolestrol, gula_darah_sewaktu, gula_darah_puasa, gula_darah_2_jam_setelah_makan, diagnosis_penyakit_tertentu, relasi_penyakit_dan_gejala)
 
                 base64_pdf = b64encode(file_pdf).decode("utf-8")
-                pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="400" type="application/pdf">'
+                pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="400" type="application/pdf">'
 
                 st.markdown(pdf_display, unsafe_allow_html=True)
 
