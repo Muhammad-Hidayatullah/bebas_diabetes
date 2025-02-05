dd786c9754281205b0f8ceb03008ea3114bc7732
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-05 15:53:51 +0700

    Update 7_test.py

--- pages/7_test.py
+++ pages/7_test.py
@@ -3,17 +3,17 @@ from streamlit_pdf_viewer import pdf_viewer
 
 
 # Declare variable.
-if 'pdf_ref' not in ss:
-    ss.pdf_ref = None
+if 'pdf_ref' not in st.session_state:
+    st.session_state.pdf_ref = None
 
 
 # Access the uploaded ref via a key.
 st.file_uploader("Upload PDF file", type=('pdf'), key='pdf')
 
-if ss.pdf:
-    ss.pdf_ref = ss.pdf  # backup
+if st.session_state.pdf:
+    st.session_state.pdf_ref = st.session_state.pdf  # backup
 
 # Now you can access "pdf_ref" anywhere in your app.
-if ss.pdf_ref:
-    binary_data = ss.pdf_ref.getvalue()
+if st.session_state.pdf_ref:
+    binary_data = st.session_state.pdf_ref.getvalue()
     pdf_viewer(input=binary_data, width=700)
