148a7c7c3cbbf496719a5cd37fc31f415140bc5f
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-10 17:36:22 +0700

    Update 7_test.py

--- pages/7_test.py
+++ pages/7_test.py
@@ -1,14 +1,4 @@
 import streamlit as st
-from fpdf import FPDF
-import datetime
-from assets import database as db
-import time
-from base64 import b64encode
-import re
-import pandas as pd
-
-
-import streamlit as st
 import pandas as pd
 
 # Sample DataFrame
