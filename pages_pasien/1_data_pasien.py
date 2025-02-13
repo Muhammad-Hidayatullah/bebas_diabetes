15fedd64d2903935ae71a16d6ef4ec2cf5fcdd47
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-13 09:59:15 +0700

    Update 1_data_pasien.py

--- pages_pasien/1_data_pasien.py
+++ pages_pasien/1_data_pasien.py
@@ -89,10 +89,7 @@ if st.session_state.lanjut == 2:
         
 
         pekerjaan = st.selectbox("Masukkan pekerjaan baru: ", options=st.session_state.pekerjaan_pekerjaan, index=st.session_state.pekerjaan_pekerjaan.index(st.session_state.pekerjaan)) 
-        st.write(pekerjaan)
-        if pekerjaan == "Lainnya":
-            pekerjaan_lainnya = st.text_input("Pekerjaan: ")
-            pekerjaan = pekerjaan_lainnya
+        
         
         tanggal_lahir = st.date_input("Masukkan tanggal lahir: (y-m-d)", min_value=datetime.date(1900, 1, 1), max_value=datetime.datetime.now(), value=st.session_state.tanggal_lahir)
         
