eb366d91e35fc3d7698c224ef0227453177add90
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-13 19:24:45 +0700

    Update 3_login_admin.py

--- pages/3_login_admin.py
+++ pages/3_login_admin.py
@@ -1,29 +1,40 @@
 import streamlit as st
 from assets import database as db
 import time
-from fpdf import FPDF
-import io
 
 
-st.subheader("ADMIN")
-admin_df = db.fetch_admin()
-st.dataframe(admin_df)
-
-pilihan_admin = st.selectbox("Pilih Opsi untuk Admin", ("Tambah Admin", "Hapus Admin"))
-if pilihan_admin == "Tambah Admin":
+if "logged_in_admin" not in st.session_state:
+    st.session_state.logged_in_admin = False
+    st.session_state.username = ""
+    
+if not st.session_state.logged_in_admin:
+    st.title("Halaman Login Admin")
+    
+    # Input username dan password hanya terlihat jika belum login
     input_username = st.text_input("Masukkan username admin:")
-    input_nama = st.text_input("Masukkan nama: ")
     input_password = st.text_input("Masukkan password:", type="password")
-    if st.button(label="Tambah"):
-        db.insert_admin(input_username, input_nama, input_password)
-        time.sleep(2)
-        st.rerun()
     
-if pilihan_admin == "Hapus Admin":
-    kol_id_admin = admin_df[admin_df["username_admin"] != "Admin"]["username_admin"]
+    if st.button(label="Login"):
+        # Validasi login
+        if db.check_admin(input_username, input_password) == True:
     
-    pilih_username = st.selectbox("Pilih Admin untuk dihapus", options=kol_id_admin, index=0)
-    if st.button("Hapus"):
-        db.hapus_admin(pilih_username)
-        time.sleep(2)
-        st.rerun()
+            st.session_state.logged_in_admin = True
+            st.session_state.masuk_website = "Admin"
+            st.session_state.username = input_username
+            st.success(f"Login berhasil! Selamat datang, {input_username}.")
+            time.sleep(2)
+            st.rerun()
+            
+        else:
+            st.error("Username atau password salah.")
+            
+
+    
+    
+
+
+    
+
+
+    
+    
