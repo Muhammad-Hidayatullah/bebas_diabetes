e8ce8ef82b6f0301936d22c7f9b211418538de4f
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-14 10:22:16 +0700

    Update 1_home_admin.py

--- pages_admin/1_home_admin.py
+++ pages_admin/1_home_admin.py
@@ -1,5 +1,30 @@
 import streamlit as st
 
+st.session_state.style_tabel = """
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        font-family: Arial, sans-serif;
+        font-size: 12px;
+    }
+    th {
+        background-color: green;
+        color: white;
+        padding: 10px;
+        text-align: left;
+    }
+    td {
+        background-color: white;
+        padding: 10px;
+        border: 1px solid #ddd;
+        text-align: left;
+    }
+    tr:nth-child(even) td {
+        background-color: #f9f9f9; /* Light gray for alternating rows */
+    }
+</style>
+"""
 
 st.title("SELAMAT DATANG ADMIN")
 
