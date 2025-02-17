9869277aaaf0688bcf52624a418c103b4b85240e
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-17 15:57:43 +0700

    Update 3_login_admin.py

--- pages/3_login_admin.py
+++ pages/3_login_admin.py
@@ -6,27 +6,33 @@ import time
 if "logged_in_admin" not in st.session_state:
     st.session_state.logged_in_admin = False
     st.session_state.username = ""
-    
-if not st.session_state.logged_in_admin:
-    st.title("Halaman Login Admin")
-    
-    # Input username dan password hanya terlihat jika belum login
-    input_username = st.text_input("Masukkan username admin:")
-    input_password = st.text_input("Masukkan password:", type="password")
-    
-    if st.button(label="Login"):
-        # Validasi login
-        if db.check_admin(input_username, input_password) == True:
-    
-            st.session_state.logged_in_admin = True
-            st.session_state.masuk_website = "Admin"
-            st.session_state.username = input_username
-            st.success(f"Login berhasil! Selamat datang, {input_username}.")
-            time.sleep(2)
-            st.rerun()
+
+with st.form("login-admin"):
+    if not st.session_state.logged_in_admin:
+        st.title("Halaman Login Admin")
+        
+        # Input username dan password hanya terlihat jika belum login
+        input_username = st.text_input("Masukkan username admin:")
+        input_password = st.text_input("Masukkan password:", type="password")
+        
+        if st.form_submit_button(label="Login"):
+            # Validasi login
+            if db.check_admin(input_username, input_password) == True:
+        
+                st.session_state.logged_in_admin = True
+                st.session_state.masuk_website = "Admin"
+                st.session_state.username = input_username
+                st.success(f"Login berhasil! Selamat datang, {input_username}.")
+                time.sleep(2)
+                st.rerun()
+                
+            else:
+                st.error("Username atau password salah.")
             
-        else:
-            st.error("Username atau password salah.")
+
+    
+    
+
             
 
     
