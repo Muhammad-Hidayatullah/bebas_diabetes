05b7126d77b3222768bdf9a8609f92e05f219c35
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-02-24 07:40:46 +0700

    Update 2_pemeriksaan_kesehatan.py

--- pages_pasien/2_pemeriksaan_kesehatan.py
+++ pages_pasien/2_pemeriksaan_kesehatan.py
@@ -154,7 +154,7 @@ def buat_laporan():
     pdf.set_font("Arial", size=18, style="B")
     pdf.cell(200, 15, txt="LAPORAN HASIL PEMERIKSAAN ", ln=True, align='C')
 
-    pdf.ln(1) 
+    pdf.ln(5) 
 
     pdf.set_font("Arial", size=13, style="B")
     pdf.cell(75, 10, txt="Data Pasien", ln=True)
