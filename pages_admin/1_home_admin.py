71df75178d1d16347cdca240149295fd6c2c6f40
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-03-03 19:57:51 +0700

    Update 1_home_admin.py

--- pages_admin/1_home_admin.py
+++ pages_admin/1_home_admin.py
@@ -37,7 +37,7 @@ st.markdown(
     f"""
     <div style="
         width: 150px;
-        padding: 2px;
+        padding: 5px;
         border-radius: 2px;
         background-color: #f0f2f6;
         text-align: center;
