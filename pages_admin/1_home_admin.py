3fb33b70bb39e38e13cf41f1518461c8f8921c4a
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-03-03 19:51:56 +0700

    Update 1_home_admin.py

--- pages_admin/1_home_admin.py
+++ pages_admin/1_home_admin.py
@@ -36,8 +36,8 @@ user_total = 10
 st.markdown(
     f"""
     <div style="
-        padding: 20px;
-        border-radius: 10px;
+        padding: 5px;
+        border-radius: 5px;
         background-color: #f0f2f6;
         text-align: center;
         box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
