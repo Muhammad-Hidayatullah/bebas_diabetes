a6adb42de04be7eb659992e9e033b85842fff965
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-03-04 10:40:08 +0700

    Update 1_home.py

--- pages/1_home.py
+++ pages/1_home.py
@@ -1,7 +1,31 @@
 import streamlit as st
 from assets import database as db
 
-
+st.session_state.style_tabel_aturan = """
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        font-family: Arial, sans-serif;
+        font-size: 12px;
+    }
+    th {
+        background-color: green;
+        color: white;
+        padding: 10px;
+        text-align: left;
+    }
+    td {
+        background-color: white;
+        padding: 10px;
+        border: 1px solid #ddd;
+        text-align: left;
+    }
+    tr:nth-child(even) td {
+        background-color: #f9f9f9;
+    }
+</style>
+"""
 
 
 
